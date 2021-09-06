35aff1f5e7dfafa26faf94e1aedff17670d68005
Ritesh Patil <riteshpatil9482@gmail.com>
2021-09-06 10:19:05 +0530

    main repo added

---
+++ src/client/modules/my/app/app.js
@@ -0,0 +1,22 @@
+import { LightningElement } from 'lwc';
+const defaultData = {
+    Name:'',
+    Email:'',
+    Mobile:''
+}
+export default class App extends LightningElement {
+    //local property
+    formData = defaultData
+
+    //method on form change
+    formchange(event){
+        const {name,value} = event.target
+        this.formData = {...this.formData, [name]:value}
+    }
+
+    //method on submit
+    checkInHandler(event){
+        event.preventDefault()
+        console.log(this.formData)
+    }
+}
